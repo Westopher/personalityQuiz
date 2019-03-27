944ca7c32fa0ef88a9dcc24e9029d8385614f52d
West Kraemer <kraemerwest@gmail.com>
2019-03-27 11:28:26 -0400

    add question array

--- personalityQuiz/QuestionData.swift
+++ personalityQuiz/QuestionData.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Questions {
+struct Question {
     var text: String
     var type: ResponseType
     var answers: [Answer]
@@ -25,4 +25,17 @@ struct Answer {
 
 enum AnimalType: Character {
     case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
 }
